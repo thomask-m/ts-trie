fe848fb921a1a286a8eab04e0baa5f0bd2f90d16
Thomas Kim <kimthomas109@gmail.com>
2019-09-27 14:00:36 -0700

    Integrate fp-ts to library

--- src/ts-trie.ts
+++ src/ts-trie.ts
@@ -1,63 +1,75 @@
+import { Option, some, none, toNullable, toUndefined, isSome, getOrElse } from 'fp-ts/lib/Option'
+import { pipe } from 'fp-ts/lib/pipeable'
+
 interface TrieNode {
-  children: { [key: string]: TrieNode };
-  value: string | null;
+  children: { [key: string]: TrieNode }
+  value: Option<string>
 }
 
 export default class Trie {
-  root: TrieNode;
+  root: TrieNode
   constructor() {
     this.root = {
       children: {},
-      value: '',
-    };
-  }
-
-  find(key: string): string | null {
-    const node = this.findNode(key);
-    return node && node.value;
-  }
-
-  findNode(key: string): TrieNode | null {
-    let node = this.root;
-    for (let i = 0; i < key.length; i++) {
-      const c = key[i];
-      node = node.children[c] || null;
+      value: none
     }
-    return node;
+  }
+
+  find(key: string): Option<string> {
+    const node = toNullable(this.findNode(key))
+    return node ? node.value : none
+  }
+
+  findNode(key: string): Option<TrieNode> {
+    let node = this.root
+    for (let i = 0; i < key.length; i++) {
+      const c = key[i]
+      if (!node.children[c]) {
+        return none
+      }
+      node = node.children[c]
+    }
+    return some(node)
   }
 
   insert(key: string, value: string) {
-    let node = this.root;
+    let node = this.root
     for (let i = 0; i < key.length; i++) {
-      const c = key[i];
+      const c = key[i]
       if (!node.children[c]) {
         node.children[c] = {
           children: {},
-          value: null,
-        };
+          value: none
+        }
       }
-      node = node.children[c];
+      node = node.children[c]
     }
-    node.value = value;
+    node.value = some(value)
   }
 
   match(key: string): string[] {
-    let matches: string[] = [];
-    let node = this.findNode(key);
-    let nextNodes: TrieNode[] = [];
-    while(node) {
-      Object.keys(node.children).forEach((c) => {
-        if (node && node.children[c]) {
-          nextNodes.push(node.children[c]);
-        }
-      });
+    let matches: string[] = []
+    let nodeOption = this.findNode(key)
+    let node = toUndefined(nodeOption)
+    let nextNodes: TrieNode[] = []
 
-      if (node && node.value) {
-        matches.push(node.value);
+    while (node) {
+      Object.keys(node.children).forEach(c => {
+        if (node && node.children[c]) {
+          nextNodes.push(node.children[c])
+        }
+      })
+
+      if (node && isSome(node.value)) {
+        const value = pipe(
+          node.value,
+          getOrElse(() => '')
+        )
+        matches.push(value)
       }
 
-      node = nextNodes.pop() || null;
+      node = nextNodes.pop()
     }
-    return matches;
+    return matches
   }
-};
+}
--- test/ts-trie.test.ts
+++ test/ts-trie.test.ts
@@ -1,55 +1,56 @@
-import Trie from '../src/ts-trie';
+import { toNullable } from 'fp-ts/lib/Option'
+import Trie from '../src/ts-trie'
 
 /**
  * Trie test
  */
 describe('Trie test', () => {
   it('Trie is instantiable', () => {
-    expect(new Trie()).toBeInstanceOf(Trie);
-  });
+    expect(new Trie()).toBeInstanceOf(Trie)
+  })
 
   it('Trie.find() works as expected', () => {
-    const trie = new Trie();
-    trie.insert('asdf', 'Thomas');
-    expect(trie.find('asdf')).toEqual('Thomas');
-    expect(trie.find('a')).toEqual(null);
-  });
+    const trie = new Trie()
+    trie.insert('asdf', 'Thomas')
+    expect(toNullable(trie.find('asdf'))).toEqual('Thomas')
+    expect(toNullable(trie.find('a'))).toEqual(null)
+  })
 
   it('Trie.insert() multiple calls', () => {
-    const trie = new Trie();
-    const testKeys = ['asdf', 'bde', 'asdfgh', 'as'];
+    const trie = new Trie()
+    const testKeys = ['asdf', 'bde', 'asdfgh', 'as']
     for (let i = 0; i < testKeys.length; i++) {
-      trie.insert(testKeys[i], testKeys[i]);
+      trie.insert(testKeys[i], testKeys[i])
     }
 
     for (let i = 0; i < testKeys.length; i++) {
-      expect(trie.find(testKeys[i])).toEqual(testKeys[i]);
+      expect(toNullable(trie.find(testKeys[i]))).toEqual(testKeys[i])
     }
-  });
+  })
 
   it('Trie.insert() negative test', () => {
-    const trie = new Trie();
-    const testKeys = ['asdf', 'bde', 'asdfgh', 'as'];
+    const trie = new Trie()
+    const testKeys = ['asdf', 'bde', 'asdfgh', 'as']
     for (let i = 0; i < testKeys.length; i++) {
-      trie.insert(testKeys[i], testKeys[i]);
+      trie.insert(testKeys[i], testKeys[i])
     }
-    expect(trie.find('asd')).toEqual(null);
-    expect(trie.find('a')).toEqual(null);
-    expect(trie.find('b')).toEqual(null);
-    expect(trie.find('bd')).toEqual(null);
-    expect(trie.find('asdfg')).toEqual(null);
-  });
+    expect(toNullable(trie.find('asd'))).toEqual(null)
+    expect(toNullable(trie.find('a'))).toEqual(null)
+    expect(toNullable(trie.find('b'))).toEqual(null)
+    expect(toNullable(trie.find('bd'))).toEqual(null)
+    expect(toNullable(trie.find('asdfg'))).toEqual(null)
+  })
 
   it('Trie.match() works as expected', () => {
-    const trie = new Trie();
-    const testKeys = ['asdf', 'bde', 'asdfgh', 'as'];
+    const trie = new Trie()
+    const testKeys = ['asdf', 'bde', 'asdfgh', 'as']
     for (let i = 0; i < testKeys.length; i++) {
-      trie.insert(testKeys[i], testKeys[i]);
+      trie.insert(testKeys[i], testKeys[i])
     }
-    expect(trie.match('asd')).toEqual(['asdf', 'asdfgh']);
-    expect(trie.match('b')).toEqual(['bde']);
-    expect(trie.match('f')).toEqual([]);
-    expect(trie.match('asdfgha')).toEqual([]);
-    expect(trie.match('ad')).toEqual([]);
-  });
-});
+    expect(trie.match('asd')).toEqual(['asdf', 'asdfgh'])
+    expect(trie.match('b')).toEqual(['bde'])
+    expect(trie.match('f')).toEqual([])
+    expect(trie.match('asdfgha')).toEqual([])
+    expect(trie.match('ad')).toEqual([])
+  })
+})
